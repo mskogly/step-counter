2be65d6cfcd3bb895a4ea086202f87eab7feb613
Morten Skogly <morten.skogly@gmail.com>
2021-09-02 18:17:40 +0200

    Startfiler for MakeCode-prosjektet

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onGesture(Gesture.Shake, function () {
+    step += 1
+    led.stopAnimation()
+})
+let step = 0
+basic.showNumber(0)
+basic.forever(function () {
+    basic.showNumber(step)
+})
